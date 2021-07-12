400afa1a153b1ebc30071a22494f8768a2c18d4d
Matheus Breder <matheus.breder@outlook.com.br>
2021-07-12 19:54:37 -0300

    Aulas 1 & 2 pt. 2

--- src/pages/NewRoom.tsx
+++ src/pages/NewRoom.tsx
@@ -10,35 +10,37 @@ import '../styles/auth.scss';
 // import { AuthContext } from '../contexts/AuthContext';
 
 export function NewRoom() {
+    // const {user} = useAuth();
 
-  // const {user} = useContext(AuthContext);
-
-  return (
-    <div id="page-auth">
-      <aside>
-        <img src={illustrationImg} alt="Ilustração simbolizando perguntas e respostas." />
-        <strong>Crie salas de Q&amp;A ao vivos</strong>
-        <p>Tire as dúvidas da sua audiência em tempo real</p>
-      </aside>
-      <main>
-        <div className="main-content">
-          <img src={logoImg} alt="Letmeask" />
-          <h2>Criar uma nova sala</h2>
-          <form>
-            <input 
-              type="text" 
-              placeholder="Nome da sala"
-              name="" id="" 
-            />
-            <Button type="submit">
-              Criar sala
-            </Button>
-          </form>
-          <p>
-            Quer entrar em uma sala existente? <Link to="/">Clique aqui</Link>
-          </p>
+    return (
+        <div id="page-auth">
+            <aside>
+                <img
+                    src={illustrationImg}
+                    alt="Ilustração simbolizando perguntas e respostas."
+                />
+                <strong>Crie salas de Q&amp;A ao vivos</strong>
+                <p>Tire as dúvidas da sua audiência em tempo real</p>
+            </aside>
+            <main>
+                <div className="main-content">
+                    <img src={logoImg} alt="Letmeask" />
+                    <h2>Criar uma nova sala</h2>
+                    <form>
+                        <input
+                            type="text"
+                            placeholder="Nome da sala"
+                            name=""
+                            id=""
+                        />
+                        <Button type="submit">Criar sala</Button>
+                    </form>
+                    <p>
+                        Quer entrar em uma sala existente?{" "}
+                        <Link to="/">Clique aqui</Link>
+                    </p>
+                </div>
+            </main>
         </div>
-      </main>
-    </div>
-  )
+    );
 }
