d6473e41067f3345d8a0b75aa9039d42de3efc3a
Matheus Breder <matheus.breder@outlook.com.br>
2021-07-17 16:55:26 -0300

    Aula 3

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,30 +1,53 @@
-import { useHistory } from 'react-router-dom';
-import { useAuth } from '../hooks/useAuth';
+import {FormEvent, useState} from "react";
+import {useHistory} from "react-router-dom";
+import {useAuth} from "../hooks/useAuth";
 
-import illustrationImg from '../assets/images/illustration.svg';
-import logoImg from '../assets/images/logo.svg';
-import googleIconImg from '../assets/images/google-icon.svg';
+import illustrationImg from "../assets/images/illustration.svg";
+import logoImg from "../assets/images/logo.svg";
+import googleIconImg from "../assets/images/google-icon.svg";
 
-import { Button } from '../components/Button';
+import {Button} from "../components/Button";
 
-import '../styles/auth.scss';
+import "../styles/auth.scss";
+import {database} from "../services/firebase";
 
 export function Home() {
   const history = useHistory();
-  const { user, signInWithGoogle } = useAuth();
+  const {user, signInWithGoogle} = useAuth();
+  const [roomCode, setRoomCode] = useState("");
 
   async function handleCreateRoom() {
     if (!user) {
       await signInWithGoogle();
     }
 
-    history.push('/rooms/new');
+    history.push("/rooms/new");
+  }
+
+  async function handleJoinRoom(event: FormEvent) {
+    event.preventDefault();
+
+    if (roomCode.trim() === "") {
+      return;
+    }
+
+    const roomRef = await database.ref(`rooms/${roomCode}`).get();
+
+    if (!roomRef.exists()) {
+      alert("Room does not exists.");
+      return;
+    }
+
+    history.push(`/rooms/${roomCode}`);
   }
 
   return (
     <div id="page-auth">
       <aside>
-        <img src={illustrationImg} alt="Ilustração simbolizando perguntas e respostas." />
+        <img
+          src={illustrationImg}
+          alt="Ilustração simbolizando perguntas e respostas."
+        />
         <strong>Crie salas de Q&amp;A ao vivos</strong>
         <p>Tire as dúvidas da sua audiência em tempo real</p>
       </aside>
@@ -36,18 +59,17 @@ export function Home() {
             Crie sua sala com o Google
           </button>
           <div className="separator">ou entre em uma sala</div>
-          <form>
-            <input 
-              type="text" 
+          <form onSubmit={handleJoinRoom}>
+            <input
+              type="text"
               placeholder="Digite o código da sala"
-              name="" id="" 
+              onChange={(event) => setRoomCode(event.target.value)}
+              value={roomCode}
             />
-            <Button type="submit">
-              Entrar na sala
-            </Button>
+            <Button type="submit">Entrar na sala</Button>
           </form>
         </div>
       </main>
     </div>
-  )
+  );
 }
